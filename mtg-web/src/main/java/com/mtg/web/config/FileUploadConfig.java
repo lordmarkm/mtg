0b9b173a6d3096ee1d55600618498b52aa2682dc
Mark <lordmarkm@yahoo.com>
2013-06-06 00:47:28 +0800

    dsfasdf

---
+++ mtg-web/src/main/java/com/mtg/web/config/FileUploadConfig.java
@@ -0,0 +1,18 @@
+package com.mtg.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+
+@Configuration
+public class FileUploadConfig {
+
+	@Bean
+	public MultipartResolver multipartResolver() {
+		CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+		multipartResolver.setMaxUploadSize(1048576); //1 Mb
+		return multipartResolver;
+	}
+	
+}
