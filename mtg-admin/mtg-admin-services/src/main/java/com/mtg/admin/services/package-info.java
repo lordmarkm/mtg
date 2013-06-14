d7da18d96eafa46a28d89446811a8fbf22bb57db
mbmartinez <mbmartinez@exist.com>
2013-06-14 18:25:04 +0800

    add comments to a mtg-admin-services pkg

---
+++ mtg-admin/mtg-admin-services/src/main/java/com/mtg/admin/services/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package should be for granting mod status, banning users, etc.
+ * Card and Expansion operations are best left to the MtG :: Commons services
+ */
+package com.mtg.admin.services;
